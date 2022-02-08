8cd7e5f4f2fd9f76004637ceef1b63393ecaffcf
Cole Gibbs <colegibbs0@gmail.com>
2022-02-08 14:29:06 -0800

    added welcome message and guessing game into

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//welcome message
+let firstName = prompt('What is your name?');
+alert(`Hey ${firstName}, it's great to have you visiting my website!`);
+
+//guessing game
+alert('Before I let you onto my website your going to play a guessing game to learn some facts about me. Read? LETS GO!');
+
+//first question
+let answerOne = prompt('Did I go to college for Chemistry?').toLowerCase().trim();
+if(answerOne === 'yes' || answerOne === 'y'){
+  
+}
